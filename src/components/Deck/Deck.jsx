d6ac7ad32fd525de494c679614efe2cdfe9033ab
emilyaikens <emily.c.aikens@gmail.com>
2022-10-07 16:30:19 -0700

    touch deck.jsx in components and add pseudocode

---
+++ src/components/Deck/Deck.jsx
@@ -0,0 +1,9 @@
+
+//using the id of the deck, render the cards that are inside of this deck
+
+export default function Deck () {
+    return (
+        // this deck will need to be a link
+        <div>deck</div>
+    )
+}
