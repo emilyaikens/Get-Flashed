134a4bd1caac817b1dbfd60d52cd9767aa0cc002
emilyaikens <emily.c.aikens@gmail.com>
2022-10-07 16:23:06 -0700

    add pseudocode to deckindex

--- src/pages/DeckIndexPage/DeckIndexPage.jsx
+++ src/pages/DeckIndexPage/DeckIndexPage.jsx
@@ -1,9 +1,12 @@
 
 
 export default function DeckIndexPage() {
+//import my decks from the database
 
     return (
         <>
+            {/* if i have decks
+                show all of the decks */}
             <h1>Deck Index Page aka my decks</h1>
         </>
     );
