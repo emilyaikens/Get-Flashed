a1640b2c9bfd0070a8bd89f9a2a3302b3fa65530
emilyaikens <emily.c.aikens@gmail.com>
2022-10-07 17:11:32 -0700

    add link to deck which takes user to displaydeckpage. Will need to add id to route later

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -1,10 +1,9 @@
-
+import { Link } from 'react-router-dom';
 //using the id of the deck, render the cards that are inside of this deck
 
 export default function Deck () {
     return (
-        // this deck will need to be a link to the DisplayDeck page.
-        //with deck id!!!
-        <div>deck</div>
+        // this link will need to be updated with id!!
+        <Link to='/displaydeck'>Deck</Link>
     )
 }
