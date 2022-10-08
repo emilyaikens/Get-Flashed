5c530c214f8b892972f5519615673e495520a15c
emilyaikens <emily.c.aikens@gmail.com>
2022-10-07 17:46:12 -0700

    create ManageDeckPage with pseudocode, need to make link from DisplayDeckpage to here

---
+++ src/pages/ManageDeckPage/ManageDeckPage.jsx
@@ -0,0 +1,14 @@
+import NewCardForm from '../../components/NewCardForm/NewCardForm';
+
+//Name of deck
+//Make new card form
+//List of existing cards
+
+export default function ManageDeckPage() {
+    return (
+        <>
+        <h1>Manage Deck Page</h1>
+        <NewCardForm />
+        </>
+    )
+}
