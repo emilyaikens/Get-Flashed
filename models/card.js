f76d5b9590256bed40890c810561b7467d335c73
emilyaikens <emily.c.aikens@gmail.com>
2022-10-07 18:10:23 -0700

    add card model with first draft code

---
+++ models/card.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    public: {
+        type: Boolean,
+        required: true
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Card', cardSchema);
