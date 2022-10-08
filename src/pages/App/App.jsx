4c944dc47f0fc2685b9920cf4692d016d06ba42b
emilyaikens <emily.c.aikens@gmail.com>
2022-10-07 17:55:04 -0700

    add ManageDeckPage to app.js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -7,6 +7,7 @@ import NavBar from '../../components/NavBar/NavBar';
 import NewDeckPage from '../NewDeckPage/NewDeckPage';
 import DeckIndexPage from '../DeckIndexPage/DeckIndexPage';
 import DeckDetailsPage from '../DeckDetailsPage/DeckDetailsPage';
+import ManageDeckPage from '../ManageDeckPage/ManageDeckPage';
 
 function App() {
     // set the user by calling getUser function
@@ -20,7 +21,9 @@ function App() {
                     <Routes>
                         <Route path="/deck/new" element={<NewDeckPage/>}/>
                         <Route path="/mydecks" element={<DeckIndexPage/>}/>
+                        {/* below: WILL NEED ID ADDED TO PATH */}
                         <Route path='/deckdetails' element={< DeckDetailsPage/>} />
+                        <Route path='/managedeck' element={< ManageDeckPage/>} />
                     </Routes>
                 </>
                 :
