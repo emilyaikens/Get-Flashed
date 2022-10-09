805717966183ac84d0283f54e18a65e6ec628204
emilyaikens <emily.c.aikens@gmail.com>
2022-10-09 10:35:36 -0700

    new deck form is created, with handlechange and handlesubmit. Handle submit only triggers an alert

--- src/components/NewDeckForm/NewDeckForm.jsx
+++ src/components/NewDeckForm/NewDeckForm.jsx
@@ -1,9 +1,50 @@
+import { useState } from "react";
 
 export default function NewDeckForm() {
     //create new deck that belongs to this user
     //once form is created, redirect to the "manage deck" page
     //so that user can add cards to the deck
+
+    const [state, setState] = useState({
+        name: "",
+        error: "",
+    });
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        try {
+            const formData = { ...state };
+            delete formData.error;
+            // const deck = await createDeck(formData);
+            alert("button clicked")
+        } catch {
+            setState({
+                error: "Sign Up Failed - Try Again",
+            });
+        }
+    };
+
+    const handleChange = (evt) => {
+        setState({
+        ...state,
+        [evt.target.name]: evt.target.value,
+        error: "",
+        });
+    };
+
     return (
-        <div>New Deck Form</div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>Deck Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    value={state.name}
+                    onChange={handleChange}
+                />
+                {/* add checkbox for public later */}
+                <button type="Submit">Create</button>
+            </form>
+        </>
     )
 }
