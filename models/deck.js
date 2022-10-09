5536e554906f7cc358d697f59f8c4bd07ecd9167
emilyaikens <emily.c.aikens@gmail.com>
2022-10-09 12:03:39 -0700

    change models. Delete card.js, move cardSchema into deck.js, it is now embedded. Add user requirements for deckSchema

--- models/deck.js
+++ models/deck.js
@@ -1,8 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const cardSchema = require('./card')
+
+const cardSchema = new Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true,
+});
 
 const deckSchema = new Schema({
+    user: {
+        type: Schma.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     name: {
         type: String,
         required: true
@@ -12,7 +29,8 @@ const deckSchema = new Schema({
         required: true,
         default: false,
     },
-    cards: [cardSchema]
+    cards: [cardSchema],
+    user: {type:Schema.Types.ObjectId, ref:'User'}
     }, {
         timestamps: true,
     });
