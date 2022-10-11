4f27f000c80bdc65c99f963f8d4d1afe1af767c3
emilyaikens <emily.c.aikens@gmail.com>
2022-10-11 10:33:51 -0700

    cards usestate and useeffect function in deckdetails page. Need to create getAllCards function in utilities/decks-api

--- src/pages/DeckDetailsPage/DeckDetailsPage.jsx
+++ src/pages/DeckDetailsPage/DeckDetailsPage.jsx
@@ -1,10 +1,24 @@
-import Card from '../../components/Card/Card';
 import { useParams } from "react-router-dom";
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getAllCards } from '../../utilities/decks-api';
+import Card from '../../components/Card/Card';
 
 //button onclick, redirect to the ManageDeckPage route WITH ID
 
 export default function DeckDetailsPage() {
+
     let id = useParams().id;
+
+    const [cards, setCards] = useState([]);
+
+    useEffect(function () {
+        async function getCards() {
+            const myCards = await getAllCards(id);
+            setCards(myCards);
+        }
+        getCards();
+    }, []);
 
     return (
         <>
