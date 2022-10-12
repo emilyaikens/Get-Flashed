f98cc0847d7ff2329f5c137a9e927d3ca5662e7e
emilyaikens <emily.c.aikens@gmail.com>
2022-10-12 10:09:27 -0700

    add buttons and handle functions to cardList

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,12 +1,28 @@
 
 export default function CardList({card, index}) {
 
+    function handleEdit(evt) {
+        evt.preventDefault();
+        alert("edit button")
+    };
+
+    function handleDelete(evt) {
+        evt.preventDefault();
+        alert("delete button")
+    };
+
     return (
         <>
-        <div>Question:</div>
-        <div>{card.question}</div>
-        <div>Answer:</div>
-        <div>{card.answer}</div>
+        <div>
+            <div>Question:</div>
+            <div>{card.question}</div>
+            <div>Answer:</div>
+            <div>{card.answer}</div>
+        </div>
+        <div>
+            <button onClick={handleEdit}>edit</button>
+            <button onClick={handleDelete}>delete</button>
+        </div>
         <br/>
         </>
     )
