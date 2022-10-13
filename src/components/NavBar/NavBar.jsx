f7f8ff30d941ec099d340cfc42b76b6b203f639e
emilyaikens <emily.c.aikens@gmail.com>
2022-10-13 16:26:00 -0700

    responsive navbar. Need to change colors

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,10 @@
 import {Link} from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
-import './NavBar.css'
+//import './NavBar.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavBar({user, setUser}) {
 
@@ -10,16 +14,35 @@ export default function NavBar({user, setUser}) {
     }
 
     return (
-        <nav>
-            <>
-            <Link to="/">My Decks</Link>
-            &nbsp; | &nbsp;
-            <Link to="/deck/new">Create Deck</Link>
-            &nbsp; | &nbsp;
-            <Link to="/browse">Browse All Decks</Link>
-            &nbsp; | &nbsp;
-            <Link to={""} onClick={handleLogOut}>Logout</Link>
-            </>
-        </nav>
+        <Navbar bg="light" expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand href="/">Get Flashed</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">My Decks</Nav.Link>
+                        <Nav.Link href="/deck/new">Create Deck</Nav.Link>
+                        <Nav.Link href="/browse">Browse All Decks</Nav.Link>
+                        
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Link href={""} onClick={handleLogOut}>Logout</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container> 
+        </Navbar>
     )
+    // return (
+    //     <>
+    //     <nav>
+    //                     <Link to="/">My Decks</Link>
+    //                     &nbsp; | &nbsp;
+    //                     <Link to="/deck/new">Create Deck</Link>
+    //                     &nbsp; | &nbsp;
+    //                     <Link to="/browse">Browse All Decks</Link>
+    //                     &nbsp; | &nbsp;
+    //                     <Link to={""} onClick={handleLogOut}>Logout</Link>
+    //     </nav>
+    //     </>
+    // )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './pages/App/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
