0e5eebe4b01b1a7a9366ffeecb5a594fe48e0019
emilyaikens <emily.c.aikens@gmail.com>
2022-10-14 18:43:30 -0700

    style alldecks and notmydeck with same as deckindex and decks

--- src/components/NotMyDeck/NotMyDeck.jsx
+++ src/components/NotMyDeck/NotMyDeck.jsx
@@ -1,11 +1,24 @@
 import { Link } from 'react-router-dom';
 
-export default function NotMyDeck({deck}) {
+export default function NotMyDeck({deck, index}) {
+
+    const colors = ['#8bc0a6', '#c4e3d4', '#f7dcb5', '#f5caac', '#f2a098'];
+
+    let cardIndex = index
+    if (index > 4 && index < 9) {
+        cardIndex = index - 5
+    } else if (index > 8 && index < 14) {
+        cardIndex = index - 10
+    } else if (index > 13 && index < 19) {
+        cardIndex = index - 15
+    } else if (index > 18) {
+        cardIndex = Math.floor(Math.random() * 4);
+    };
 
     return (
         <>
             <Link to={`/deckdetails/${deck._id}`}>
-                <button className="deck">{deck.name}</button>
+                <button style={{backgroundColor:`${colors[cardIndex]}`}} className="deck">{deck.name}</button>
             </Link>
         </>
     )
