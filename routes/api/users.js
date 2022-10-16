a4e3afefe0191578471e0985b954ccf0fcd1c247
emilyaikens <emily.c.aikens@gmail.com>
2022-10-15 22:04:03 -0700

    organize routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
-
-// call the middleware
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 // GET /api/users/check-token
 router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
-
 // POST : http://localhost:3001/api/users
 router.post('/', usersCtrl.create);
-
 // POST : http://localhost:3001/api/users/login
 router.post('/login', usersCtrl.login);
 
