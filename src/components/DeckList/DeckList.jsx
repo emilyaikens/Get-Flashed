deadf15910e1b2d04bccc672f85d19cfb396c7c5
emilyaikens <emily.c.aikens@gmail.com>
2022-10-15 22:17:19 -0700

    comment deckList

--- src/components/DeckList/DeckList.jsx
+++ src/components/DeckList/DeckList.jsx
@@ -3,6 +3,8 @@ import './DeckList.css';
 
 export default function DeckList({decks, setDeckName}) {
 
+    //decks and setDeckName useStates come from DeckIndexPage
+
     const myDecks = decks.map((value, index) => 
         <Deck deck={value} key={value._id} index={index} setDeckName={setDeckName}/>)
 
