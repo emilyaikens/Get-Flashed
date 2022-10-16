fd514989d50b9697df792435f36c4a80d70534b5
emilyaikens <emily.c.aikens@gmail.com>
2022-10-15 22:01:15 -0700

    comments in controllers/users.js

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -2,49 +2,45 @@ const User = require('../../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
 
-async function create(req, res) {
-    try {
-        const user = await User.create(req.body);
-        const token = createJWT(user);
-        return res.json(token);
-    } catch (error) {
-        res.status(400).json(error);
+//create a token-based user using signup form body
+    async function create(req, res) {
+        try {
+            const user = await User.create(req.body);
+            const token = createJWT(user);
+            return res.json(token);
+        } catch (error) {
+            res.status(400).json(error);
+        }
     }
-}
 
-function createJWT(user) {
-    return jwt.sign(
-        {user},
-        process.env.SECRET,
-        {expiresIn: '24h'}
-    );
-}
-
-async function login(req, res) {
-    try {
-        // first fine the user
-        const user = await User.findOne({email: req.body.email});
-        // if there is no user in the throw an error
-        if (!user) throw new Error();
-        // get user submitted password, and compare with the object found on the db
-        // apple, HSH3ychs
-        const match = await bcrypt.compare(req.body.password, user.password);
-        // no, match? send error
-        if (!match) throw new Error();
-        // there is a match! we found the user
-        // create, and return the token
-        const token = createJWT(user);
-        res.json(token);
-    } catch (err) {
-        // something went wrong, send status 400 error
-        res.status(400).json('Bad Credentials');
+//create a token for user that expires in 24 hours
+    function createJWT(user) {
+        return jwt.sign(
+            {user},
+            process.env.SECRET,
+            {expiresIn: '24h'}
+        );
     }
-}
 
-function checkToken(req, res) {
-    console.log('req.user', req.user);
-    res.json(req.exp);
-}
+//find user by email. get user submitted password, and compare with the object found on the db
+    async function login(req, res) {
+        try {
+            const user = await User.findOne({email: req.body.email});
+            if (!user) throw new Error();
+            const match = await bcrypt.compare(req.body.password, user.password);
+            if (!match) throw new Error();
+            const token = createJWT(user);
+            res.json(token);
+        } catch (err) {
+            res.status(400).json('Bad Credentials');
+        }
+    }
+
+//send expiry
+    function checkToken(req, res) {
+        console.log('req.user', req.user);
+        res.json(req.exp);
+    }
 
 module.exports = {
     create,
