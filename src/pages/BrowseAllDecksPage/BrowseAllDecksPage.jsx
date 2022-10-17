301c013279955cecab8c88e0af914734662d2f54
emilyaikens <emily.c.aikens@gmail.com>
2022-10-16 17:42:02 -0700

    remove dead code from browsealldeckspage. Decks are rendering odd, not in a straight line

--- src/pages/BrowseAllDecksPage/BrowseAllDecksPage.jsx
+++ src/pages/BrowseAllDecksPage/BrowseAllDecksPage.jsx
@@ -1,64 +1,3 @@
-// import { useState } from 'react';
-// import { useEffect } from 'react';
-// import { getAllDecks } from '../../utilities/decks-api';
-// import NotMyDeck from '../../components/NotMyDeck/NotMyDeck';
-// import SearchBar from '../../components/SearchBar/SearchBar';
-// import './BrowseAllDecksPage';
-
-// export default function BrowseAllDecks() {
-
-//     //this use state keeps track of public decks
-
-//     const [allDecks, setAllDecks] = useState([]);
-
-//     //this function fetches public decks from all users and updates use state above
-
-//     useEffect(function () {
-//         async function showAllDecks() {
-//             const allDecks = await getAllDecks();
-//             setAllDecks(allDecks);
-//         }
-//         showAllDecks();
-//     }, []);
-
-//     //variables and logic below:
-//     //if more than 10 decks are returned from the function above, only map the first 10
-//     //otherwise, map all of them
-
-//     let browseDecks = "";
-//     let noDeck = ""
-
-//     if (allDecks.length > 10) {
-//         browseDecks = allDecks.slice([0], [9]).map((value, index) => 
-//             <NotMyDeck deck={value} key={value._id} index={index}/>)
-//     } else {
-//         browseDecks = allDecks.map((value, index) => 
-//             <NotMyDeck deck={value} key={value._id} index={index}/>)
-//     };
-
-//     //below: if there are no decks returned by the showAllDecks/getAllDecks functions
-//     //then show the "sorry there are no decks..." message
-//     //otherwise, show the decks that have been fetched from the database
-
-//     return (
-//         <>
-//         <div className="topper"></div>
-//         {allDecks.length > 0 ?
-//             <>
-//                 <SearchBar setAllDecks={setAllDecks}/>
-//                 <div>{browseDecks}</div>
-//                 <div>{noDeck}</div>
-//             </>
-//         :
-//         <>
-//             <h3 style={{fontFamily:'Peralta'}}>"Sorry, there are no public decks that match your search"</h3>
-//             <img className="cowboy" src="https://i.imgur.com/g7A8rGD.png" />
-//         </>
-//         }
-//         </>
-//     )
-// }
-
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { getAllDecks } from '../../utilities/decks-api';
@@ -68,7 +7,11 @@ import './BrowseAllDecksPage';
 
 export default function BrowseAllDecks() {
 
+    //this use state keeps track of public decks
+
     const [allDecks, setAllDecks] = useState([]);
+
+    //this function fetches public decks from all users and updates use state above
 
     useEffect(function () {
         async function showAllDecks() {
@@ -78,17 +21,13 @@ export default function BrowseAllDecks() {
         showAllDecks();
     }, []);
 
+    //variables and logic below:
+    //if more than 10 decks are returned from the function above, only map the first 10
+    //otherwise, map all of them
+
     let browseDecks = "";
-    let noDeck = ""
 
-    if (allDecks.length === 0) {
-        noDeck = <div>
-                    <img className="index-cowboy" src="https://i.imgur.com/h9DRnp1.png" />
-                    <div>"Sorry, there are no public decks that match your search"</div>
-                </div>
-    }
-
-    if (allDecks.length > 9) {
+    if (allDecks.length > 10) {
         browseDecks = allDecks.slice([0], [9]).map((value, index) => 
             <NotMyDeck deck={value} key={value._id} index={index}/>)
     } else {
@@ -96,12 +35,24 @@ export default function BrowseAllDecks() {
             <NotMyDeck deck={value} key={value._id} index={index}/>)
     };
 
+    //below: if there are no decks returned by the showAllDecks/getAllDecks functions
+    //then show the "sorry there are no decks..." message
+    //otherwise, show the decks that have been fetched from the database
+
     return (
         <>
-            <div className="topper"></div>
-            <SearchBar allDecks={allDecks} setAllDecks={setAllDecks}/>
-            <div>{browseDecks}</div>
-            <div>{noDeck}</div>
+        <div className="topper"></div>
+        <SearchBar setAllDecks={setAllDecks}/>
+        {allDecks.length > 0 ?
+            <>
+                <div>{browseDecks}</div>
+            </>
+        :
+        <>
+            <h3 style={{fontFamily:'Peralta'}}>"Sorry, there are no public decks that match your search"</h3>
+            <img className="cowboy" src="https://i.imgur.com/g7A8rGD.png" />
+        </>
+        }
         </>
     )
-}
+};
