a0a02ee2210ed09999ab8d855472eb6548366cee
emilyaikens <emily.c.aikens@gmail.com>
2022-10-23 22:23:01 -0700

    rerender card color determining function in deck.jsx

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -6,20 +6,16 @@ export default function Deck ({deck, index, setDeckName}) {
     //colors array and logic below determine color of the deck
 
     const colors = ['#8bc0a6', '#c4e3d4', '#f7dcb5', '#f5caac', '#f2a098'];
-    const ends = [50, 16, 27, 38, 49];
+    const ends = ['50', '16', '27', '38', '49'];
 
     let cardIndex = index
-    if (index > 4 && index < 9) {
-        cardIndex = index - 5
-    } else if (index > 8 && index < 14) {
-        cardIndex = index - 10
-    } else if (index > 13 && index < 19) {
-        cardIndex = index - 15
-    } else if (index > 18) {
-        cardIndex = Math.floor(Math.random() * 4);
-    };
+    let n = index.toString().split('');
 
-    let n = index.split();
+    for (let i = 0; i < 5; i++) {
+        if (ends[i].includes(n[n.length - 1])) {
+            cardIndex = i;
+        };
+    };
 
     //each deck is a button that links to DeckDetailsPage
 
