6bbf458fcd2c055e82c630c383b01229ecc7c844
emilyaikens <emily.c.aikens@gmail.com>
2022-10-26 20:36:21 -0700

    color buttons for deck in place, the use state updates every time next, back or flip is clicked. Will need to move use state somewhere else...

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,9 +12,46 @@ export default function CardList({card}) {
         setFlip(!flip);
     }
 
+    //cardColor and handle functions below change the color of the cards
+
+    const [cardColor, setCardColor] = useState("white");
+
+    //const colors = ['#8bc0a6', '#c4e3d4', '#f7dcb5', '#f5caac', '#f2a098'];
+
+    function handleGreen(evt) {
+        evt.preventDefault();
+        setCardColor("#c4e3d4");
+    };
+
+    function handleYellow(evt) {
+        evt.preventDefault();
+        setCardColor("#f7dcb5");
+    };
+
+    function handleOrange(evt) {
+        evt.preventDefault();
+        setCardColor("#f5caac");
+    };
+
+    function handleRed(evt) {
+        evt.preventDefault();
+        setCardColor("#f2a098");
+    };
+
+    function handleWhite(evt) {
+        evt.preventDefault();
+        setCardColor("white");
+    };
+
         return (
             <>
-            <div className={`card ${flip ? "flip" : ""}`}>
+            <button className="color-button" onClick={handleGreen} style={{color: "#c4e3d4", backgroundColor: "#c4e3d4"}}>o</button>
+            <button className="color-button" onClick={handleYellow} style={{color: "#f7dcb5", backgroundColor: "#f7dcb5"}}>o</button>
+            <button className="color-button" onClick={handleOrange} style={{color: "#f5caac", backgroundColor: "#f5caac"}}>o</button>
+            <button className="color-button" onClick={handleRed} style={{color: "#f2a098", backgroundColor: "#f2a098"}}>o</button>
+            <button className="color-button" onClick={handleWhite} style={{color: "white", backgroundColor: "white"}}>o</button>
+            <div><br/></div>
+            <div className={`card ${flip ? "flip" : ""}`} style={{backgroundColor: cardColor}}>
                 <div className="front">{card.question}</div>
                 <div className="back">{card.answer}</div>
             </div>
